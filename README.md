1f48036a54007a28171a0339495e908162c9f48a
Veeled <vedhneeli@gmail.com>
2023-01-16 20:52:27 +1000

    Added .innerText to displayScore to remove the compounding of text after each player selection

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # rock-paper-scissors+
-Creating a game which is playable via the chrome console. 
+Creating a game which is playable.
--- game.js
+++ game.js
@@ -1,5 +1,15 @@
+
+const rock = document.querySelector('.rock')
+const paper = document.querySelector('.paper')
+const scissors = document.querySelector('.scissors')
+const displayScore = document.querySelector('.score')
+const playerTally = document.querySelector('.playerScore')
+const compTally = document.querySelector('.compScore')
+//const output = document.querySelector('.total') --> not really needed
+
 let playerScore = 0;
 let compScore = 0;
+
 
 const getComputerChoice = () => {
     const myArrayChoice = ['rock', 'paper', 'scissors']
@@ -7,44 +17,86 @@ const getComputerChoice = () => {
     return getRandom
 }
 
+//display.innerText to display results without appending it to the next line. Replaces the text as opposed to compounding. 
+
 function playRound(playerSelection, autoSelection) {
     if (playerSelection === autoSelection) {
-        return 'TIED';
+        //const p = document.createElement('p')
+        displayScore.innerText = `TIED, You Both Picked ${playerSelection}`
+        //displayScore.appendChild(p)
     } else if (playerSelection == 'rock' && autoSelection == 'paper') {
         compScore++
-        return 'You Lose'
+        //const p = document.createElement('p')
+        displayScore.innerText = 'You Lose'
+        //displayScore.appendChild(p)
     } else if (playerSelection == 'scissors' && autoSelection == 'paper') {
         playerScore++
-        return 'You Win'
+        //const p = document.createElement('p')
+        displayScore.innerText = 'You Win'
+        //displayScore.appendChild(p)
     } else if (playerSelection == 'rock' && autoSelection == 'scissors') {
         playerScore++
-        return 'You Win'
+        //const p = document.createElement('p')
+        displayScore.innerText = 'You Win'
+        //displayScore.appendChild(p)
     } else if (playerSelection == 'paper' && autoSelection == 'scissors') {
         compScore++    
-        return 'You Lose'
+        //const p = document.createElement('p')
+        displayScore.innerText = 'You Lose'
+        //displayScore.appendChild(p)
     } else if (playerSelection == 'scissors' && autoSelection == 'rock') {
         compScore++
-        return 'You Lose'
+        //const p = document.createElement('p')
+        displayScore.innerText = 'You Lose'
+        //displayScore.appendChild(p)
     } else if (playerSelection == 'paper' && autoSelection == 'rock') {
         playerScore++
-        return 'You Win'
+        //const p = document.createElement('p')
+        displayScore.innerText = 'You Win'
+        //displayScore.appendChild(p)
+    } else {
+        //const p = document.createElement('p')
+        displayScore.innerText = 'Nothing Selected'
+        //displayScore.appendChild(p)
     }
 }
 
-function game () {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt('Select Either Rock, Paper, Scissors!').toLowerCase();
-        const autoSelection = getComputerChoice();
-        console.log(playRound(playerSelection, autoSelection));
+
+rock.addEventListener('click', () => {
+    const autoSelection = getComputerChoice();
+    const playerSelection = 'rock';
+    playRound(playerSelection, autoSelection)
+    playerTally.innerText = `Player Score:  ${playerScore}`
+    compTally.innerText = `Computer Score: ${compScore}`
+    winner (playerScore, compScore)
+})
+paper.addEventListener('click', () => {
+    const autoSelection = getComputerChoice();
+    const playerSelection = 'paper';
+    playRound(playerSelection, autoSelection)
+    playerTally.innerText = `Player Score:  ${playerScore}`
+    compTally.innerText = `Computer Score: ${compScore}`
+    winner (playerScore, compScore)
+})
+scissors.addEventListener('click', () => {
+    const autoSelection = getComputerChoice();
+    const playerSelection = 'scissors';
+    playRound(playerSelection, autoSelection)
+    playerTally.innerText = `Player Score:  ${playerScore}`
+    compTally.innerText = `Computer Score: ${compScore}`
+    winner (playerScore, compScore)
+})
+
+function winner (playerScore, compScore) {
+    if (playerScore == 5) {
+        displayScore.innerText = "You Have WON THE GAME BIG MAN"
+    } else if (compScore == 5) {
+        displayScore.innerText = "The COMPUTER is the VICTOR TODAY!"
     }
-if (playerScore > compScore) {
-    return 'You WON Against the Computer, Congrats!'
-} else if (playerScore < compScore) {
-    return 'You LOST, need some more practice there buddy, ALSO YOU SUCK BIG TIME DUMBASS HAHAHHAHAHHAHHAHAHHHA'
-} else {
-    return 'You TIED Against the Computer!'
 }
-}
-console.log(game())
+
+
+
+
 
 
